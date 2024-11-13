b1fca7e30d13daafc0dad0689079a76b3b7bd2bd
madhumitasugandhi <madhumitasugandhi@gmail.com>
2024-11-13 13:43:54 +0530

    Footer.js added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import "./index.css"
 
-import Home from './views/Home/Home';
-import About from './views/About/About';
+import Home from './Views/Home/Home';
+import About from './Views/About/About';
 import Contact from './Views/Contact/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
